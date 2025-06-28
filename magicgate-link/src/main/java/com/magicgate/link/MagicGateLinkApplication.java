fde150da93132970c1d3cc03ad4dff0e14e77869
yangyangSheepCoding <codedog39@163.com>
2025-06-29 01:11:30 +0800

    starter

---
+++ magicgate-link/src/main/java/com/magicgate/link/MagicGateLinkApplication.java
@@ -0,0 +1,11 @@
+package com.magicgate.link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicGateLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MagicGateLinkApplication.class, args);
+    }
+}
